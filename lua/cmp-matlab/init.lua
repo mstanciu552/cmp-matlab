abe1214321990b98e3a203a4bd9eab0e9d34d92b
mstanciu552 <mstanciu552@gmail.com>
2021-10-27 11:57:52 +0300

    added signal sources

--- lua/cmp-matlab/init.lua
+++ lua/cmp-matlab/init.lua
@@ -1,42 +1,47 @@
 local source = {}
 
----Source constructor.
 source.new = function()
 	local self = setmetatable({}, { __index = source })
 	self.your_awesome_variable = 1
 	return self
 end
 
----Return the source is available or not.
----@return boolean
 function source:is_available()
 	local filetype = vim.bo.filetype
 	return filetype == "matlab"
 end
 
----Return the source name for some information.
 function source:get_debug_name()
 	return "cmp_matlab"
 end
 
----Invoke completion (required).
----  If you want to abort completion, just call the callback without arguments.
----@param params cmp.SourceCompletionApiParams
----@param callback fun(response: lsp.CompletionResponse|nil)
-function source:complete(params, callback)
-	callback(require("cmp-matlab.source"))
+local merge_sources = function(sources, source)
+	-- TODO Write this function
 end
 
----Resolve completion item that will be called when the item selected or before the item confirmation.
----@param completion_item lsp.CompletionItem
----@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:complete(params, callback)
+	local sources = {}
+	local n = 0
+
+	local matlab_source = require("cmp-matlab.source")
+	local signal_source = require("cmp-matlab.signal")
+
+	for _, entry in ipairs(matlab_source) do
+		sources[n] = entry
+		n = n + 1
+	end
+	for _, entry in ipairs(signal_source) do
+		sources[n] = entry
+		n = n + 1
+	end
+
+	callback(sources)
+end
+
 function source:resolve(completion_item, callback)
 	callback(completion_item)
 end
 
----Execute command that will be called when after the item confirmation.
----@param completion_item lsp.CompletionItem
----@param callback fun(completion_item: lsp.CompletionItem|nil)
 function source:execute(completion_item, callback)
 	callback(completion_item)
 end
